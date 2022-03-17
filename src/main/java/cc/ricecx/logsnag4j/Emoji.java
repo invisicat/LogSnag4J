fd87c1f3ff511d5b9d8406680da1f729e156595b
RiceCX <andyl5463@gmail.com>
2022-03-16 17:49:29 -0700

    add: javadocs for Emoji.java

--- src/main/java/cc/ricecx/logsnag4j/Emoji.java
+++ src/main/java/cc/ricecx/logsnag4j/Emoji.java
@@ -1,17 +1,35 @@
 package cc.ricecx.logsnag4j;
 
+/**
+ * A simple class to differentiate between an emoji and a normal string.
+ * @author ricecx
+ * @since 1.0
+ */
 public class Emoji {
 
     private final String emoji;
 
+    /**
+     * Converts a string into an emoji.
+     * @param emoji The string to convert.
+     */
     private Emoji(String emoji) {
         this.emoji = emoji;
     }
 
+    /**
+     * Converts a string into an emoji.
+     * @param emoji The string to convert.
+     * @return The emoji.
+     */
     public static Emoji of(String emoji) {
         return new Emoji(emoji);
     }
 
+    /**
+     * Gets the emoji.
+     * @return The emoji.
+     */
     public String getEmoji() {
         return emoji;
     }
